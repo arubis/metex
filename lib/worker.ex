74a4b62ea3c0de5a88ea5fb60de2bf82fe92a783
Dylan Fitzgerald <dylanfitzgerald@learnzillion.com>
2017-08-13 16:22:45 -0600

    Naïve implementation

---
+++ lib/worker.ex
@@ -0,0 +1,38 @@
+defmodule Metex.Worker do
+  def temperature_of(location) do
+    result = url_for(location)
+    |> HTTPoison.get
+    |> parse_response
+
+    case result do
+      {:ok, temp} ->
+        "#{location}: #{temp}°C"
+      :error ->
+        "#{location} not found"
+    end
+  end
+
+  defp url_for(location) do
+    location = URI.encode(location)
+    "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
+  end
+
+  defp parse_response({:ok, %HTTPoison.Response{body: body}}) do
+    body
+    |> JSON.decode!
+    |> compute_temperature
+  end
+
+  # defp parse_response(_), do: :error
+
+  defp compute_temperature(json) do
+    try do
+      temp = (json["main"]["temp"] - 273.15) |> Float.round(1)
+      {:ok, temp}
+    rescue
+      _ -> :error
+    end
+  end
+
+  defp apikey, do: "f087bb7b03be3ea9d1c57d97989572d7"
+end
